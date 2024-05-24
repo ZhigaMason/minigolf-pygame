e8a924efe02f512165c5fcc7dc1988076d860ab9
Maksym Khavil <khavimak@fit.cvut.cz>
2024-05-24 19:20:00 +0200

    (ft) add 8th level

--- app/src/levels/lvl_07.py
+++ app/src/levels/lvl_07.py
@@ -1,9 +1,16 @@
 """ Implementing 8th level """
-from util.config import GRID_SIZE, DEBUG
+from util.config import SCREEN_SIZE, CELL_SIZE, DEBUG
+from util.level_gen import generate_level
 from src.levels_list import levels
 
-from gui.cell.walls import Wall, SilentWall
-from gui.cell.floors import Grass, Ice, Sand
+
+lvl = levels[7]
+with open(__file__[:-2] + 'txt','r') as f:
+    generate_level(f, lvl)
+
+lvl.set_initial_pos((7,5), (31, 5), (7, 24), (31, 24))
+
+lvl.set_hole((SCREEN_SIZE[0] // 2 - CELL_SIZE // 2, SCREEN_SIZE[1] // 2 + CELL_SIZE // 2),grid=False)
 
 if DEBUG:
     print(__name__, 'loaded')
