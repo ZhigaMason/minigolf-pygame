cee00f5c0384818a8bbb5748ccb242c4a98ae4b5
Maksym Khavil <khavimak@fit.cvut.cz>
2024-05-24 12:50:46 +0200

    (ft) write 1st level

--- app/src/levels/lvl_00.py
+++ app/src/levels/lvl_00.py
@@ -1,15 +1,26 @@
 """ Implementing 1st level """
-from util.config import GRID_SIZE, DEBUG
+from util.config import GRID_SIZE, SCREEN_SIZE, DEBUG
 from src.levels_list import levels
+from util.level_config import print_quote
 
 from gui.cell.walls import Wall, SilentWall
-from gui.cell.floors import Grass, Ice, Sand
+from gui.cell.floors import Sand
 
-levels[0].set_initial_pos((10,10), (6,10), (7,10), (8,10))
-levels[0].set_cells_by_type(SilentWall, [ (i, 3) for i in range(1, GRID_SIZE[0] - 1)])
-levels[0].set_cells_by_type(Ice, [ (i, j) for i in range(4, 8) for j in range(10, 25)])
-levels[0].set_cells_by_type(Sand, [ (i, j) for i in range(10, 15) for j in range(18, 23)])
-levels[0].set_hole((15, 15))
+lvl = levels[0]
+GRD_W, GRD_H = GRID_SIZE
 
+lvl.set_cells_by_type(Wall, ( (i, j) for i in range(10) for j in range(9, GRD_H - 8) ))
+lvl.set_cells_by_type(Wall, ( (i, j) for i in range(GRD_W - 10, GRD_W) for j in range(9, GRD_H - 8) ))
+lvl.set_cells_by_type(Wall, ( (i, j) for i in range(GRD_W) for j in (8, GRD_H - 9)))
+
+lvl.set_cells_by_type(Sand, ( (i, j) for i in range(GRD_W) for j in range(1, 8)))
+lvl.set_cells_by_type(Sand, ( (i, j) for i in range(GRD_W) for j in range(GRD_H - 8, GRD_H - 1)))
+
+
+print_quote('PUSH BALL', (0, 1), lvl, cell=SilentWall)
+print_quote('INTO HOLE', (0, GRD_H - 8), lvl, cell=SilentWall)
+
+lvl.set_initial_pos(*( (i, j) for i in (11, GRD_W - 12) for j in (10, GRD_H - 11 )))
+lvl.set_hole((SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2), grid=False)
 if DEBUG:
     print(__name__, 'loaded')
---
+++ app/util/level_config.py
@@ -0,0 +1,28 @@
+""" Config file for level creating """
+from gui.level.level import Level
+
+LETTERS = {
+    'D' : (5, (*((0, j) for j in range (0, 7)), (1, 0), (2, 0), (1, 6), (2, 6), *((3, j) for j in range(1, 6)))),
+    'R' : (5, (*((0, j) for j in range (0, 7)), (1, 0), (2, 0), (1, 3), (2, 3), (3, 1), (3, 2), (3, 4), (3, 5), (3, 6))),
+    'A' : (5, (*((0, j) for j in range (1, 7)), (1, 0), (2, 0), (1, 3), (2, 3), *((3, j) for j in range (1, 7)))),
+    'G' : (5, (*((0, j) for j in range (1, 6)), (1, 0), (2, 0), (1, 6), (2, 6), *((3, j) for j in (1, 4, 5)), (2, 4))),
+    ' ' : (1, ()),
+    'B' : (5, (*((0, j) for j in range (0, 7)), *( (i, j) for i in (1,2) for j in (0, 3, 6)), *((3, j) for j in (1,2,4,5)))),
+    'L' : (5, (*((0, j) for j in range (0, 7)), *((i, 6) for i in range(4)))),
+    'H' : (5, (*((0, j) for j in range (0, 7)), (1, 3), (2, 3), *((3, j) for j in range (0, 7)),)),
+    'E' : (5, (*((0, j) for j in range (0, 7)), *((i, j) for i in (1, 2, 3) for j in (0,3,6)))),
+    'T' : (6, (*((2, j) for j in range (1, 7)), *((i, 0) for i in range(5)))),
+    'I' : (4, (*((1, j) for j in range(1, 6)),*((i,j) for i in range(3) for j in (0, 6)))),
+    'O' : (5, (*((i, j) for i in (0, 3) for j in range(1, 6)), *((i,j) for i in (1, 2) for j in (0, 6)))),
+    'N' : (5, (*((i, j) for i in (0, 3) for j in range(0, 7)), (1,1), (1,2), (2,3), (2, 4))),
+    'P' : (5, (*((0, j) for j in range(7)), *( (i,j) for i in (1,2) for j in (0, 3)), (3,1), (3,2))),
+    'U' : (5, (*((i, j) for i in (0, 3) for j in range(6)), (1, 6), (2,6))),
+    'S' : (5, (*((0, j) for j in (1, 2, 5)), *((i,j) for i in (1,2) for j in (0, 3, 6)),*((3, j) for j in (1, 4, 5))))
+}
+def print_quote(quote : str, ofs : tuple[int, int], level : Level, *, cell):
+    """ Prints quote onto level """
+    stack_ofs = 0
+    for char in quote:
+        add_ofs = LETTERS[char][0]
+        level.set_cells_by_type(cell, ( (ofs[0] + stack_ofs + x, ofs[1] + y) for x, y in LETTERS[char][1]))
+        stack_ofs += add_ofs
