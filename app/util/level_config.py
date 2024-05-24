69c7737b3d39f87f22a40fe3cccdb3b972145846
Maksym Khavil <khavimak@fit.cvut.cz>
2024-05-24 13:21:29 +0200

    (ft) update level config

--- app/util/level_config.py
+++ app/util/level_config.py
@@ -1,5 +1,6 @@
 """ Config file for level creating """
 from gui.level.level import Level
+import util.config as cfg
 
 LETTERS = {
     'D' : (5, (*((0, j) for j in range (0, 7)), (1, 0), (2, 0), (1, 6), (2, 6), *((3, j) for j in range(1, 6)))),
@@ -17,7 +18,8 @@ LETTERS = {
     'N' : (5, (*((i, j) for i in (0, 3) for j in range(0, 7)), (1,1), (1,2), (2,3), (2, 4))),
     'P' : (5, (*((0, j) for j in range(7)), *( (i,j) for i in (1,2) for j in (0, 3)), (3,1), (3,2))),
     'U' : (5, (*((i, j) for i in (0, 3) for j in range(6)), (1, 6), (2,6))),
-    'S' : (5, (*((0, j) for j in (1, 2, 5)), *((i,j) for i in (1,2) for j in (0, 3, 6)),*((3, j) for j in (1, 4, 5))))
+    'S' : (5, (*((0, j) for j in (1, 2, 5)), *((i,j) for i in (1,2) for j in (0, 3, 6)),*((3, j) for j in (1, 4, 5)))),
+    'C' : (5, (*((0, j) for j in range(1, 6)), *( (i,j) for i in (1,2) for j in (0, 6)), (3,1), (3,5))),
 }
 def print_quote(quote : str, ofs : tuple[int, int], level : Level, *, cell):
     """ Prints quote onto level """
@@ -26,3 +28,5 @@ def print_quote(quote : str, ofs : tuple[int, int], level : Level, *, cell):
         add_ofs = LETTERS[char][0]
         level.set_cells_by_type(cell, ( (ofs[0] + stack_ofs + x, ofs[1] + y) for x, y in LETTERS[char][1]))
         stack_ofs += add_ofs
+
+GRD_W, GRD_H = cfg.GRID_SIZE
