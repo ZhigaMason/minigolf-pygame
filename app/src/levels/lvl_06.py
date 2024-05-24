336cfc8535719193b80c55897968ffdfeebd4c88
Maksym Khavil <khavimak@fit.cvut.cz>
2024-05-24 18:24:21 +0200

    (ft) add 7th level

--- app/src/levels/lvl_06.py
+++ app/src/levels/lvl_06.py
@@ -1,9 +1,17 @@
 """ Implementing 7th level """
-from util.config import GRID_SIZE, DEBUG
+from util.config import SCREEN_SIZE, DEBUG
+from util.level_gen import generate_level
+from util.level_config import GRD_W, GRD_H
 from src.levels_list import levels
 
-from gui.cell.walls import Wall, SilentWall
-from gui.cell.floors import Grass, Ice, Sand
+
+lvl = levels[6]
+with open(__file__[:-2] + 'txt','r') as f:
+    generate_level(f, lvl)
+
+lvl.set_initial_pos(*((i, j) for i in (2, GRD_W - 3) for j in (2, GRD_H - 3)))
+
+lvl.set_hole((SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 2),grid=False)
 
 if DEBUG:
     print(__name__, 'loaded')
