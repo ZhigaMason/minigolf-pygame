c1cd8ca7ed8a230414472c701812f26b55080e2f
Maksym Khavil <khavimak@fit.cvut.cz>
2024-05-24 17:31:36 +0200

    (ft) implement 6th level

--- app/src/levels/lvl_05.py
+++ app/src/levels/lvl_05.py
@@ -1,9 +1,16 @@
-""" Implementing 5th level """
-from util.config import GRID_SIZE, DEBUG
+""" Implementing 6th level """
+from util.config import SCREEN_SIZE, DEBUG
+from util.level_gen import generate_level
 from src.levels_list import levels
 
-from gui.cell.walls import Wall, SilentWall
-from gui.cell.floors import Grass, Ice, Sand
+
+lvl = levels[5]
+with open(__file__[:-2] + 'txt','r') as f:
+    generate_level(f, lvl)
+
+lvl.set_initial_pos(*((i, 25) for i in range(8, 12)))
+
+lvl.set_hole((SCREEN_SIZE[0] // 2 - 32, 80),grid=False)
 
 if DEBUG:
     print(__name__, 'loaded')
